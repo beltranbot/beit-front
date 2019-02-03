5dc1410c7d5999f01c3a8cd183c7e6992e729ca0
Carlos Beltran <beltranbot@gmail.com>
2019-02-03 09:44:25 -0500

    ajuste en entorno de desarrollo

--- src/config.env.js
+++ src/config.env.js
@@ -1,5 +1,6 @@
 const proxy = 'https://cors-anywhere.herokuapp.com/'
-const base_url = 'http://127.0.0.1:8000/api/'
+// const base_url = 'http://127.0.0.1:8000/api/'
+const base_url = 'http://laravel-beitech.local/api/'
 
 const use_proxy = false
 let url = ''
@@ -11,11 +12,13 @@ if (use_proxy) {
 
 const orders_url = url + 'orders'
 const customers_url = url + 'customers'
+const customer_products_url = url + 'customer-products'
 
 export {
   proxy,
   base_url,
   url,
   orders_url,
-  customers_url
+  customers_url,
+  customer_products_url
 }
